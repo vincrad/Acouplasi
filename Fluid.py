6f19de73977751267c177b29a7282a9a5f5463da
radmann <vincent.radmann@tu-berlin.de>
2020-10-06 11:29:16 +0200

    Adds: Property decorator to Fluid to calculate the speed of sound.

--- Fluid.py
+++ Fluid.py
@@ -10,20 +10,44 @@ import traitlets as tr
 import numpy as np
 from numpy import sqrt
 
+# =============================================================================
+# class Fluid(tr.HasTraits):
+#     '''
+#     Class to define the fluid in the duct.
+#     '''
+#     
+#     rho0 = tr.Float()
+#     kappa = 1.4
+#     R = 287.058     #J/kgK
+#     
+#     #c = tr.property
+#     
+#     def soundspeed(self, temp):
+#         
+#         self.c = np.sqrt(self.kappa*self.R*(temp+273.15))
+#         
+#         return self.c
+# =============================================================================
+    
 class Fluid(tr.HasTraits):
     '''
     Class to define the fluid in the duct.
     '''
     
-    rho0 = tr.Float()
-    kappa = 1.4
-    R = 287.058     #J/kgK
+    rho0 = tr.Float(1.2)
+    kappa = tr.Float(1.4)
+    R = tr.Float(287.058)   # J/kgK
+    c = tr.Float()
     
-    def soundspeed(self, temp):
+    temperatureC = tr.Float(20)
+    
+    @property
+    def c(self):
         
-        self.c = np.sqrt(self.kappa*self.R*(temp+273.15))
+        c = np.sqrt(self.kappa*self.R*(self.temperatureC+273.15))
         
-        return self.c
+        return c
+    
     
 
     
--- Test.py
+++ Test.py
@@ -22,76 +22,93 @@ from Linings import DummyLining, DummyReflection, DummyAbsorption
 
 #%% Test 6.1 - Kombination Reflexion/Absorption - Lazy
 
-f = list(range(10,260,1))
-
-TL = []
-
-temp = np.arange(20, 430, 200)
-
-for  i in temp:
-
-    fluid1 = Fluid(rho0=1.2)
-    c = fluid1.soundspeed(i)
-    
-    lining1 = DummyReflection(length=5, depth=2, height=5, medium=fluid1)
-    
-    lining2 = DummyAbsorption(length=5, depth=2, height=1, dw=0.35, medium=fluid1)
-    
-    ductelement1 = DuctElementDummy(lining=lining1, medium=fluid1)
-    
-    ductelement2 = DuctElementDummy(lining=lining2, medium=fluid1)
-    
-    duct = Duct(elements=[ductelement1, ductelement2], freq=f)
-    
-    duct.tmatrix()
-    
-    tl = duct.tl()
-    
-    TL.append(tl)
-    
-fig = plt.figure
-for i in TL:
-    plt.plot(f, i)
+# =============================================================================
+# f = list(range(10,260,1))
+# 
+# TL = []
+# 
+# temp = np.arange(20, 430, 200)
+# 
+# for  i in temp:
+# 
+#     fluid1 = Fluid(rho0=1.2)
+#     c = fluid1.soundspeed(i)
+#     
+#     lining1 = DummyReflection(length=5, depth=2, height=5, medium=fluid1)
+#     
+#     lining2 = DummyAbsorption(length=5, depth=2, height=1, dw=0.35, medium=fluid1)
+#     
+#     ductelement1 = DuctElementDummy(lining=lining1, medium=fluid1)
+#     
+#     ductelement2 = DuctElementDummy(lining=lining2, medium=fluid1)
+#     
+#     duct = Duct(elements=[ductelement1, ductelement2], freq=f)
+#     
+#     duct.tmatrix()
+#     
+#     tl = duct.tl()
+#     
+#     TL.append(tl)
+#     
+# fig = plt.figure
+# for i in TL:
+#     plt.plot(f, i)
+# =============================================================================
 
 
 
 #%% Test 7 - Lazy Evaluation
 
+temp = 20
+
+f = list(range(10,260,1))
+#f = list(np.arange(10,260,10))
+
+fluid1 = Fluid()
+#c = fluid1.soundspeed(temp)
+
+lining1 = DummyReflection(length=5, depth=2, height=5, medium=fluid1)
+
+lining2 = DummyReflection(length=1, depth=2, height=1, medium=fluid1)
+
+lining3 = DummyReflection(length=4, depth=2, height=5, medium=fluid1)
+
+ductelement1 = DuctElementDummy(lining=lining1, medium=fluid1)
+
+ductelement2 = DuctElementDummy(lining=lining2, medium=fluid1)
+
+ductelement3 = DuctElementDummy(lining=lining3, medium=fluid1)
+
+duct = Duct(elements=[ductelement1, ductelement2, ductelement3], freq=f)
+
+duct.tmatrix()
+
+tl2 = duct.tl()
+
+fig = plt.figure()
+plt.plot(f, tl2)
+
+
+#%% 
+
 # =============================================================================
-# temp = 20
-# 
 # f = list(range(10,260,1))
-# #f = list(np.arange(10,260,10))
 # 
-# fluid1 = Fluid(rho0=1.2)
-# c = fluid1.soundspeed(temp)
+# fluid1 = Fluid()
 # 
 # lining1 = DummyReflection(length=5, depth=2, height=5, medium=fluid1)
 # 
-# lining2 = DummyReflection(length=1, depth=2, height=1, medium=fluid1)
-# 
-# lining3 = DummyReflection(length=4, depth=2, height=5, medium=fluid1)
-# 
 # ductelement1 = DuctElementDummy(lining=lining1, medium=fluid1)
 # 
-# ductelement2 = DuctElementDummy(lining=lining2, medium=fluid1)
-# 
-# ductelement3 = DuctElementDummy(lining=lining3, medium=fluid1)
-# 
-# duct = Duct(elements=[ductelement1, ductelement2, ductelement3], freq=f)
+# duct = Duct(elements=[ductelement1], freq=f)
 # 
 # duct.tmatrix()
 # 
-# tl2 = duct.tl()
+# tl = duct.tl()
 # 
-# fig = plt.figure()
-# plt.plot(f, tl2)
+# fig = plt.plot()
+# plt.plot(f, tl)
 # =============================================================================
-
-
-
-
-
 
 
 
