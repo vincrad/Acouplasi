Refactor: Correct the calculation of impedance matrix of plate induced sound regarding j = l and j != l in SinglePlateResonator class

Linings.py
@@ -53,58 +53,98 @@ class SinglePlateResonator(PlateResonators):
         
         return np.eye(len(self.cavity.r),1)[np.newaxis, np.newaxis, :]
         
-
     
-    # method calculate the impedance matrix of the plate resonator
+    # methode calculate the impedance matrix of the plate resonator
     def zmatrix(self, M, freq):
         
-       # circular frequency and wave number
-       omega = 2*np.pi*freq
-       k0 = omega/self.cavity.medium.c
-       
-       # cavity impedance matrix
-       Zc = self.cavity.cavityimpedance(self.length, self.depth, self.j, self.l, freq)
-       
-       # impedance matrix of the plate induced sound
-       # define expanded arrays of modes
-       L = self.l[:, np.newaxis, np.newaxis, np.newaxis]
-       J = self.j[np.newaxis, :, np.newaxis, np.newaxis]
-       R = self.cavity.r[np.newaxis, np.newaxis, :, np.newaxis]
-       
-       # calculate the impedance matrix of the plate induced sound
-       Krp = -k0*M-1j*np.sqrt((1-M**2)*(R*np.pi)**2-k0**2, dtype=complex)/(1-M**2)
-       Krm = k0*M-1j*np.sqrt((1-M**2)*(R*np.pi)**2-k0**2, dtype=complex)/(1-M**2)
-       
-       x0=numpy.pi**2
-       x1=self.length**2
-       x2=Krm**2*x1
-       x3=J**2
-       x4=-x0*x3
-       x5=L**2
-       x6=-x0*x5
-       x7=x2 + x6
-       x8=J + 1
-       x9=(-1)**(J + L)
-       x10=1j*self.length
-       x11=Krm*x10
-       x12=numpy.exp(x11)
-       x13=x3 - x5
-       x14=x0*x13
-       #x15=J*k0*L*x1/x13
-       x15 = np.divide(J*k0*L*x1, x13, out=np.zeros_like(J*k0*L*x1), where=x13!=0)
-       x16=Krp**2*x1
-       x17=x16 + x6
-       x18=Krp*x10
-       x19=numpy.exp(x18)
-       
-       Zpradtemp = (2 - self.deltar)*(x15*(x12*x7*((-1)**(L + x8) + 1) + x14*((-1)**x8 + x12*x9))*numpy.exp(-x11)/(x7*(x2 + x4)) + x15*((-1)**(L + 1)*x14 + x14*x19 + x17*x19*(x9 - 1))*numpy.exp(-x18)/(x17*(x16 + x4)))/numpy.sqrt(-k0**2 + R**2*x0, dtype=complex)
-       
-       Zprad = np.sum(Zpradtemp, axis=2)*(1/2)*1j*self.length
-       
-       # calculate the total impedance matrix
-       Z = Zc+Zprad
-       
-       return Z
+        # circular frequency and wave number
+        omega = 2*np.pi*freq
+        k0 = omega/self.cavity.medium.c
+        
+        # cavity impedance matrix
+        Zc = self.cavity.cavityimpedance(self.length, self.depth, self.j, self.l, freq)
+        
+        # impedance matrix of the plate induced sound
+        # define expanded arrays of modes
+        L = self.l[:, np.newaxis, np.newaxis, np.newaxis]
+        J = self.j[np.newaxis, :, np.newaxis, np.newaxis]
+        R = self.cavity.r[np.newaxis, np.newaxis, :, np.newaxis]
+        
+        # calculate the impedance matrix of the plate induced sound
+        Krp = -k0*M-1j*np.sqrt((1-M**2)*(R*np.pi)**2-k0**2, dtype=complex)/(1-M**2)
+        Krm = k0*M-1j*np.sqrt((1-M**2)*(R*np.pi)**2-k0**2, dtype=complex)/(1-M**2)
+        
+        # for j=l
+        x0=numpy.pi**2
+        x1=self.length**2
+        x2=L**2*x0
+        x3=-x2
+        x4=Krm**2*x1 + x3
+        x5=Krm*M
+        x6=1j*self.length
+        x7=Krm*x6
+        x8=numpy.exp(x7)
+        x9=(-1)**(L + 1) + x8
+        x10=2*self.length*x2
+        x11=omega*x10
+        x12=1j*x4*x8
+        x13=omega*x1
+        x14=M*x2
+        x15=(1/2)*self.length/omega
+        x16=Krp**2*x1 + x3
+        x17=Krp*M
+        x18=(-1)**L
+        x19=x10*x17
+        x20=Krp*x6
+        x21=numpy.exp(x20)
+        x22=1j*x16*x21
+        
+        Zpll_temp = (1/2)*x6*(2 - self.deltar)*(x15*(k0 + x5)*(-Krm*x12*x13 + x10*x5*x9 + x11*x9 - x12*x14)*numpy.exp(-x7)/x4**2 + x15*(k0 - x17)*(-Krp*x13*x22 - x11*x18 + x11*x21 + x14*x22 + x18*x19 - x19*x21)*numpy.exp(-x20)/x16**2)/numpy.sqrt(-k0**2 + R**2*x0*(1 - M**2), dtype=complex)
+        
+        Zpll = np.sum(Zpll_temp*(np.identity(len(self.l))[:, :, np.newaxis, np.newaxis]), axis=2)
+        
+        # for j != l
+        x0=numpy.pi**2
+        x1=self.length**2
+        x2=Krm**2*x1
+        x3=J**2
+        x4=-x0*x3
+        x5=L**2
+        x6=-x0*x5
+        x7=x2 + x6
+        x8=Krm*M
+        x9=J + 1
+        x10=(-1)**(J + L)
+        x11=1j*self.length
+        x12=Krm*x11
+        x13=numpy.exp(x12)
+        x14=(-1)**x9 + x10*x13
+        x15=x3 - x5
+        x16=x0*x15
+        x17=omega*x16
+        x18=(-1)**(L + x9) + 1
+        x19=x13*x18*x7
+        #x20=J*L*x1/(omega*x15)
+        x20 = np.divide(J*L*x1, (omega*x15), out=np.zeros_like(J*L*x1/omega), where=(omega*x15)!=0)
+        x21=Krp**2*x1
+        x22=x21 + x6
+        x23=Krp*M
+        x24=x16*x23
+        x25=Krp*x11
+        x26=numpy.exp(x25)
+        x27=x22*x26
+        
+        Zplj_temp = (1/2)*x11*(2 - self.deltar)*(x20*(k0 + x8)*(omega*x19 + x14*x16*x8 + x14*x17 + x19*x8)*numpy.exp(-x12)/(x7*(x2 + x4)) + x20*(k0 - x23)*((-1)**L*x24 + (-1)**(L + 1)*x17 + omega*x27*(x10 - 1) + x17*x26 + x18*x23*x27 - x24*x26)*numpy.exp(-x25)/(x22*(x21 + x4)))/numpy.sqrt(-k0**2 + R**2*x0*(1 - M**2), dtype=complex)
+        
+        Zplj = np.sum(Zplj_temp, axis=2)
+        
+        # calculate the overall impedance matrix of the plate induced sound
+        Zprad = Zplj+Zpll
+        
+        # calculate the total impedance matrix of the lining
+        Z = Zc+Zprad
+        
+        return Z
    
     # method calculate the plate velocity
     def platevelocity(self, I, M, freq):